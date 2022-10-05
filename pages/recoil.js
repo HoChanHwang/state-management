a51827ebbd2c5a602fb4e552e48ad3d386e5055e
hwanghochan <hochan.hwang@wonderwall.kr>
2022-10-05 10:56:25 +0900

    recoil

--- pages/recoil.js
+++ pages/recoil.js
@@ -1,5 +1,35 @@
+import {atom, RecoilRoot, useRecoilState, useRecoilValue} from "recoil";
+
+const counterState = atom({
+    key: 'counterState',
+    default: 0
+});
+
+const Counter = () => {
+    const [counter, setCounter] = useRecoilState(counterState);
+
+    return (
+        <div>
+            <button onClick={() => setCounter(c => c - 1)}>-</button>
+            <div>{counter}</div>
+            <button onClick={() => setCounter(c => c + 1)}>+</button>
+        </div>
+    )
+}
+
+const CurrentCounter = () => {
+    const counter = useRecoilValue(counterState);
+
+    return <div>{`현재 카운터 숫자는 ${counter} 입니다.`}</div>
+}
+
 const RecoilPage = () => {
-    return <div>this is recoil page</div>;
+    return (
+        <RecoilRoot>
+            <Counter />
+            <CurrentCounter />
+        </RecoilRoot>
+    );
 }
 
 export default RecoilPage;
