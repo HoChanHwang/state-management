23eb12fccc8376e4ef2f7e6d8ba5d4db31a28b4e
hwanghochan <hochan.hwang@wonderwall.kr>
2022-10-05 12:52:44 +0900

    mobx

--- pages/mobx.js
+++ pages/mobx.js
@@ -1,5 +1,51 @@
+import {makeAutoObservable} from "mobx";
+import {observer, Provider} from "mobx-react";
+
+class Counter {
+    counter = 0;
+
+    constructor() {
+        console.log(this);
+        makeAutoObservable(this);
+    }
+
+    increase = () => {
+        this.counter += 1;
+    }
+
+    decrease = () => {
+        this.counter -= 1;
+    }
+}
+
+const CounterC = observer(({counter}) => {
+    return (
+        <div>
+            <button onClick={counter.decrease}>-</button>
+            <div>{counter.counter}</div>
+            <button onClick={counter.increase}>+</button>
+        </div>
+    )
+})
+
+const CurrentCounter = observer(({counter}) => {
+    return <div>{`현재 카운터 숫자는 ${counter.counter} 입니다.`}</div>
+})
+
+const DoubleCurrentCounter = observer(({counter}) => {
+    return <div>{`현재 카운터 숫자는 ${counter.counter * 2} 입니다.`}</div>
+})
+
 const MobxPage = () => {
-    return <div>this is mobx page</div>;
+    const myCounter = new Counter();
+
+    return (
+        <Provider>
+            <CounterC counter={myCounter} />
+            <CurrentCounter counter={myCounter} />
+            <DoubleCurrentCounter counter={myCounter} />
+        </Provider>
+    );
 }
 
 export default MobxPage;
